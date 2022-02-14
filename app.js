5b083808f1d3905bbb2690bd1ba9de75f939b20e
devnoman01 <devnoman01@gmail.com>
2022-02-14 15:07:00 +0600

    take input and display - complete

--- app.js
+++ app.js
@@ -13,3 +13,20 @@ function generatePin(){
     const pin = getPin();
     document.getElementById('display-pin').value = pin;
 }
+
+document.getElementById('key-pad').addEventListener('click', function (event) {
+    const number = event.target.innerText;
+    const displayInput = document.getElementById('typed-pin');
+
+    if(isNaN(number)){
+        if(number == 'C'){
+            displayInput.value = '';
+        }
+    }
+    else{
+        const previousInput = displayInput.value;
+        const newInput = previousInput + number
+        displayInput.value = newInput;
+    }
+    
+})
